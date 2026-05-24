aa04332974cd4ac3a84a3a8d515c358f1eef68d1
JvVireo <wialon161@gmail.com>
2026-05-24 16:07:58 +0300

    Delete src directory
    i'll add the updated "src"

--- src/NativeLibraries/Expertions/MusicPlayerException.java
+++
@@ -1,7 +0,0 @@
-package NativeLibraries.Expertions;
-
-public class MusicPlayerException extends Exception{
-    public MusicPlayerException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
